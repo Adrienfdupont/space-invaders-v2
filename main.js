275043832779dd7615f26b525563ebd9865b4c52
Adrienfdupont <adrienf.dupont@gmail.com>
2022-09-11 21:09:33 +0200

    commit

--- main.js
+++ main.js
@@ -11,30 +11,16 @@ class Entity{
         // positionnement de l'élément
         this.el.style.position = 'absolute';
         this.setX(x);
-        this.el.style.transform = 'translateX(-50%)';
+        this.el.style.transform = 'translate(-50%, 50%)';
         this.setY(y);
     }
 
+    // getters et setter relatifs à la position de l'élément
     getX(){
         return parseInt(this.el.style.left.substring(0, this.el.style.left.length - 2));
     }
     getY(){
         return parseInt(this.el.style.bottom.substring(0, this.el.style.bottom.length - 2));
-    }
-    getLeft(){
-        return this.getX() - parseInt(this.el.style.width.substring(0, this.el.style.width.length - 2) / 2);
-    }
-    getRight(){
-        return this.getX() + parseInt(this.el.style.width.substring(0, this.el.style.width.length - 2) / 2);
-    }
-    getBottom(){
-        return this.getY() + parseInt(this.el.style.height.substring(0, this.el.style.height.length - 2) / 2);
-    }
-    getTop(){
-        return this.getY() - parseInt(this.el.style.height.substring(0, this.el.style.height.length - 2) / 2);
-    }
-    getWidth(){
-        return parseInt(this.el.style.width.substring(0, this.el.style.width.length - 2));
     }
     setX(int){
         this.el.style.left = int + 'px';
@@ -42,21 +28,45 @@ class Entity{
     setY(int){
         this.el.style.bottom = int + 'px';
     }
+
+    // getters et setters relatifs aux bords de l'élément
+    getLeft(){
+        return this.getX() - this.getWidth() / 2;
+    }
+    getRight(){
+        const cssObj = window.getComputedStyle(this.el, null);   
+        const right = cssObj.getPropertyValue("right");
+        return parseInt(right.substring(0, right.length - 2)) + this.getWidth() / 2;
+    }
+    getBottom(){
+        return this.getY() - this.getWidth() / 2;
+    }
+    getTop(){
+        const cssObj = window.getComputedStyle(this.el, null);   
+        const top = cssObj.getPropertyValue("top");
+        return parseInt(top.substring(0, top.length - 2)) - this.getWidth() / 2;
+    }
+
+    // getters et setters relatifs à la taille de l'élément
+    getWidth(){
+        return parseInt(this.el.style.width.substring(0, this.el.style.width.length - 2));
+    }   
     setWidth(int){
         this.el.style.width = int + 'px';
     }
-
 }
 
 class Ship extends Entity{
 
-    constructor(imgPath){
+    constructor(imgPath, speed){
 
         let width = window.innerWidth / 20;
         let x = window.innerWidth / 2;
-        let y =10
-
+        let y = 50;
         super(imgPath, width, x, y);
+
+        this.speed = speed;
+        this.loaded = true;
         
         // contrôles clavier
         window.addEventListener('keydown', (e)=>{
@@ -64,29 +74,34 @@ class Ship extends Entity{
         })
     }
 
-    controllShip(event){
+    controllShip(e){
         // tir du missile
-        if (event.key == ' '){
+        if (e.key == ' '){
             this.shootMissile();
         }
         // déplacement vaisseau vers la gauche
-        else if (event.key == 'ArrowLeft' && this.getX() > 0 + this.getWidth() / 2){
-            this.setX(this.getX() - window.innerWidth / 100);
+        else if (e.key == 'ArrowLeft' && ship.getLeft() >= 0){
+            this.setX(this.getX() - window.innerWidth / 100 * this.speed);
         }
         // déplacement vaisseau vers la droite
-        else if (event.key == 'ArrowRight' && this.getX() < window.innerWidth - this.getWidth() / 2){
-            this.setX(this.getX() + window.innerWidth / 100);
-        }
+        else if (e.key == 'ArrowRight' && ship.getRight() >= 0){
+            this.setX(this.getX() + window.innerWidth / 100 * this.speed);      }
     }
 
     shootMissile(){
-        const missile = new Missile('./images/missile.png');
+        if (this.loaded){
+            new Missile('./images/missile.png', 5);
+
+            // rechargement du canon
+            this.loaded = false;
+            this.reload = setTimeout(() => {this.loaded = true}, 2000);
+        }
     }
 }
 
 class Missile extends Entity{
 
-    constructor(imgPath){
+    constructor(imgPath, speed){
         
         let width = window.innerWidth / 50;
         let x = ship.getX();
@@ -95,12 +110,14 @@ class Missile extends Entity{
         super(imgPath, width, x, y);
 
         this.el.style.zIndex = '-10';
-        this.animation = setInterval(()=>{this.moveMissile()}, 10);
+        this.speed = speed;
+
+        this.animation = setInterval(() => {this.moveMissile()}, 10);
     }
 
     moveMissile(){
         if (this.getY() < window.innerHeight){
-            this.setY(this.getY() + 5);
+            this.setY(this.getY() + this.speed);
         } else {
             clearInterval(this.animation);
             this.el.remove();
@@ -119,9 +136,9 @@ class Alien extends Entity{
     }
 
     moveAlienRight(){
-        if (this.getY() > 0) {
+        if (this.getTop() > 0) {
             this.animation = setInterval(()=>{
-                if (this.getRight() <= window.innerWidth){
+                if (this.getRight() > 0){
                     this.setX(this.getX() + 1);
                 } else {
                     clearInterval(this.animation);
@@ -138,7 +155,7 @@ class Alien extends Entity{
     moveAlienLeft(){
         if (this.getY() > 0){
             this.animation = setInterval(()=>{
-                if (this.getLeft() >= 0){
+                if (this.getLeft() > 0){
                     this.setX(this.getX() - 1);
                 } else {
                     clearInterval(this.animation);
@@ -153,7 +170,5 @@ class Alien extends Entity{
     }
 }
 
-const ship = new Ship('./images/ship.png');
-const alien1 = new Alien('./images/alien.png', 0, 300);
-
-console.log(alien1.getBottom());
+const ship = new Ship('./images/ship.png', 1);
+const alien1 = new Alien('./images/alien.png', 0, 500);
