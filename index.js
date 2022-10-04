3e444fe85380077e28451f77623f1cbf38dd6581
Adrienfdupont <adrienf.dupont@gmail.com>
2022-10-04 18:26:15 +0200

    z-index pop-up non pris en compte

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ Manager.displayElements();
 
 // lancement de la partie
 const playButton = document.querySelector('#play-button');
+
 playButton.onclick = ()=>{
     Manager.initGame();
+}
+
+// affichage des paramètres
+const settings = document.querySelector('#settings');
+const settingsButton = document.querySelector('#settings-button');
+
+settingsButton.onclick = () =>{
+    console.log('test');
+    settings.style.display = 'flex';
 }
